7fadae4214996f1379cd9d04fd3524e58b2b40ce
Ivan Faryga <ifaryga@gmail.com>
2021-11-07 00:35:13 +0200

    done

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,16 +1,26 @@
-import { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACT } from "../types/types";
+// import { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACT } from "../types/types";  //======= var 1
+// import { createAction } from "@reduxjs/toolkit";   //========== var 2
 
-export const addContact = (formData) => ({
-  type: ADD_CONTACT,
-  payload: formData,
-});
+//======================= 1 ========= Vanilla Redux
 
-export const deleteContact = (id) => ({
-  type: DELETE_CONTACT,
-  payload: id,
-});
+// export const addContact = (formData) => ({
+//   type: ADD_CONTACT,
+//   payload: formData,
+// });
 
-export const filterContact = (value) => ({
-  type: FILTER_CONTACT,
-  payload: value,
-});
+// export const deleteContact = (id) => ({
+//   type: DELETE_CONTACT,
+//   payload: id,
+// });
+
+// export const filterContact = (value) => ({
+//   type: FILTER_CONTACT,
+//   payload: value,
+// });
+
+//======================= 2 ========= Basic REDUX TOOLKIT
+
+// export const addContact = createAction("contacts/addContact");
+// export const deleteContact = createAction("contacts/deleteContact");
+
+// export const filterContact = createAction("contacts/filterContact");
